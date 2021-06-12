730cc177862f3c7245ff390fb45446676b9ca83c
Harri Lainio <harri.lainio@op.fi>
2021-06-12 21:08:01 +0300

    only wents thru the compiler

--- chain/chain.go
+++ chain/chain.go
@@ -9,25 +9,42 @@ type Block struct {
 	Position          int
 }
 
+func (b Block) Hash() []byte {
+	return nil // TODO: 
+}
+
 type Chain struct {
 	blocks []Block
 }
 
 type PubKey = []byte
-type Key = []byte
+type Key struct {
+	PrivKey []byte
+	PubKey
+}
+
+func (k Key) PubKeyEqual( pubKey PubKey) bool {
+	return byteEqual(k.PubKey, pubKey)
+}
+
+func (k Key) Sign(h []byte) Signature {
+	return nil // TODO: 
+}
+
 type Signature = []byte
 
 func NewChain(rootPubKey PubKey) Chain {
-	chain = Chain{blocks: make(Chain, 1, 12)}
+	chain := Chain{blocks: make([]Block, 1, 12)}
 	chain.blocks[0] = Block{
 		HashToPrev: nil,
 		InviteePubKey: rootPubKey,
 		InvitersSignature: nil,
 	}
+	return chain
 }
 
 func (c *Chain) AddBlock(invitersKey Key, inviteesPubKey PubKey, position int) {
-	assert.D.True(invitersKey.PubKey == c.LeafPubKey())
+	assert.D.True(invitersKey.PubKeyEqual(c.LeafPubKey()))
 
 	newBlock := Block{
 		HashToPrev:    c.HashToLeaf(),
@@ -44,4 +61,20 @@ func (c Chain) LeafPubKey() PubKey {
 	assert.D.True(len(c.blocks) > 0)
 
 	return c.blocks[len(c.blocks)-1].InviteePubKey
+}
+
+func (c Chain) HashToLeaf() []byte {
+	return nil // TODO:
+}
+
+func byteEqual(a, b []byte) bool {
+    if len(a) != len(b) {
+        return false
+    }
+    for i, v := range a {
+        if v != b[i] {
+            return false
+        }
+    }
+    return true
 }
