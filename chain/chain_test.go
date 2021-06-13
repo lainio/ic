a5fc55cc4f3b2ea4aaaa9bfa7faecb6b049fc66f
Harri Lainio <harri.lainio@op.fi>
2021-06-13 10:31:53 +0300

    first working test without real cryptos

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -1,8 +1,16 @@
 package chain
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewChain(t *testing.T) {
-	rootKey := 
-	c := NewChain(rootKey)
+	rootKey := Key{}
+	c := NewChain(rootKey.PubKey)
+	inviteeKey := Key{}
+	level := 1
+	c.AddBlock(rootKey, inviteeKey.PubKey, level)
+	assert.Len(t, c.blocks, 2)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/findy-network/ic
 
 go 1.16
 
-require github.com/lainio/err2 v0.6.1 // indirect
+require (
+	github.com/lainio/err2 v0.6.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
