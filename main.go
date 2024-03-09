1ae630c19a3e71e2d02e33b0611cc338dc5b4d65
Harri Lainio <harri.lainio@iki.fi>
2024-03-09 17:30:27 +0200

    lastest and meanest

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	filename := "large-file.json"
 	// Adjust the number of iterations as needed
 
+	start := time.Now() // Start the timer
+
 	// Read the JSON file outside of the timing loop
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -20,12 +22,10 @@ func main() {
 		return
 	}
 
-	start := time.Now() // Start the timer
-
 	for i := 0; i < iterations; i++ {
-		//jsonData := make([]json.RawMessage, 0, 14000) 
-		var jsonData []json.RawMessage
-		// var jsonData map[string]interface{} // Use this instead if the JSON root is an object
+		//var jsonData []json.RawMessage
+		//var jsonData map[string]interface{} // Use this instead if the JSON root is an object
+		var jsonData []Item
 		if err := json.Unmarshal(data, &jsonData); err != nil {
 			fmt.Printf("failed to unmarshal json: %v\n", err)
 			return
@@ -35,4 +35,30 @@ func main() {
 
 	duration := time.Since(start)
 	fmt.Printf("Unmarshalling took %v for %d iterations\n", duration, iterations)
+}
+
+type Item struct {
+	Id    string `json:"id"`
+	Type  string `json:"type"`
+	Actor struct {
+		Id         int    `json:"id"`
+		Login      string `json:"login"`
+		GravatarId string `json:"gravatar_id"`
+		Url        string `json:"url"`
+		AvatarUrl  string `json:"avatar_url"`
+	} `json:"actor"`
+	Repo struct {
+		Id   int    `json:"id"`
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	} `json:"repo"`
+	Payload struct {
+		Ref          string `json:"ref"`
+		RefType      string `json:"ref_type"`
+		MasterBranch string `json:"master_branch"`
+		Description  string `json:"description"`
+		PusherType   string `json:"pusher_type"`
+	} `json:"payload"`
+	Public    bool      `json:"public"`
+	CreatedAt time.Time `json:"created_at"`
 }
--- main2.go
+++ main2.go
@@ -14,7 +14,6 @@ import (
 
 func main() {
 	assert.SetDefault(assert.Plain)
-
 	defer err2.Catch(err2.Stderr)
 
 	assert.SLonger(os.Args, 1, "\n\nUsage: %v <json_file>",
@@ -25,13 +24,68 @@ func main() {
 
 	r := try.To1(os.Open(filename))
 
-	//jsonData := make([]json.RawMessage, 0, 14000)
-	var jsonData []json.RawMessage
+	ready := make(chan struct{})
+	ch := make(chan json.RawMessage, 100)
+	go func() {
+		defer err2.Catch("worker error")
 
-	try.To(json.NewDecoder(r).Decode(&jsonData))
+		count := 0
+		for d := range ch {
+			var item Item
+			try.To(json.Unmarshal(d, &item))
+			count++
+		}
+		println("count: ", count)
+		ready <- struct{}{}
+	}()
+	//var jsonData []json.RawMessage
 
-	println("jsonData len:", len(jsonData))
+	//try.To(json.NewDecoder(r).Decode(&jsonData))
+	dc := json.NewDecoder(r)
+	try.To1(dc.Token()) // read '['
+
+	count := 0
+	for dc.More() {
+		var jsonData json.RawMessage
+		//var jsonData Item
+		try.To(dc.Decode(&jsonData))
+		ch <- jsonData
+		count++
+	}
+	close(ch)
+
+	try.To1(dc.Token()) // read ']'
+
+	//println("jsonData len:", len(jsonData))
+	println("data len:", count)
 
 	duration := time.Since(start)
 	fmt.Printf("Unmarshalling took %v\n", duration)
+	<-ready
+}
+
+type Item struct {
+	Id    string `json:"id"`
+	Type  string `json:"type"`
+	Actor struct {
+		Id         int    `json:"id"`
+		Login      string `json:"login"`
+		GravatarId string `json:"gravatar_id"`
+		Url        string `json:"url"`
+		AvatarUrl  string `json:"avatar_url"`
+	} `json:"actor"`
+	Repo struct {
+		Id   int    `json:"id"`
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	} `json:"repo"`
+	Payload struct {
+		Ref          string `json:"ref"`
+		RefType      string `json:"ref_type"`
+		MasterBranch string `json:"master_branch"`
+		Description  string `json:"description"`
+		PusherType   string `json:"pusher_type"`
+	} `json:"payload"`
+	Public    bool      `json:"public"`
+	CreatedAt time.Time `json:"created_at"`
 }
