512830bf942b542434a03182e91c5c78168d1db8
Harri Lainio <harri.lainio@iki.fi>
2025-01-11 20:12:55 +0200

    new-key new format base58

--- cmds/newkey.go
+++ cmds/newkey.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/lainio/err2"
 	"github.com/lainio/err2/try"
 	"github.com/spf13/cobra"
@@ -12,7 +13,8 @@ import (
 
 var newKeyDoc = `Create a new key for enlave use.
 
-Returns a hex code 32 byte random secret to be used e.g. stateless key storage.`
+Returns a hex/base58 code 32 byte random secret to be used e.g. stateless key
+storage.`
 
 var newKeyCmd = &cobra.Command{
 	Use:   "new-key",
@@ -23,14 +25,24 @@ var newKeyCmd = &cobra.Command{
 
 		key := make([]byte, 32)
 		try.To1(rand.Read(key))
+		if wantBase58 {
+			fmt.Println(base58.Encode(key))
+			return nil
+		}
 		fmt.Println(hex.EncodeToString(key))
 
 		return nil
 	},
 }
 
+var wantBase58 bool
+
 func init() {
 	defer err2.Catch()
+
+	flags := newKeyCmd.PersistentFlags()
+	flags.BoolVarP(&wantBase58, "base58", "b", false,
+		"wants key output format to be base58 (default is hex)")
 
 	rootCmd.AddCommand(newKeyCmd)
 }
